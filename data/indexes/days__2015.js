6726b5574514b70cf5abe1bab04823add95eb0f3
Marcos Marado <mindboosternoori@gmail.com>
2025-11-17 18:12:56 +0000

    added missing yearly indexes, 2014 and 2015

---
+++ data/indexes/days__2015.js
@@ -0,0 +1,5 @@
+
+{"meta":{"descriptor":"days__2015","title":"2015","kind":"plain"},"rows":[
+	{"descriptor":"19","entries":["26a41343"],"title":"Feb 19"},{"descriptor":"27","entries":["2d0cf6a1"],"title":"Feb 27"},
+	{"descriptor":"03","entries":["ab7a7b62"],"title":"Apr 03"}
+]}
